df427d0103364b07050a5cee61f34ee7b34afcb3
Hugo Rafael <hugorafell@gmail.com>
2022-04-26 21:30:12 -0300

    Update aula 26/04/22

---
+++ Tarefas/Sistema de Reservas/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception { 
+        
+        Reserva reserva = new Reserva( new PessoaFisica("joao", "1212-81-12") , true );
+
+        System.out.println(reserva);
+        
+    }
+}
